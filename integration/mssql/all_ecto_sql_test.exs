8a2567065dc54472f8344f2c81ec7cca76b55d12
whossname <tyson.buzza@gmail.com>
2019-07-14 17:52:38 +0800

    split integration tests based on source

---
+++ integration/mssql/all_ecto_sql_test.exs
@@ -0,0 +1,13 @@
+ecto_sql = Mix.Project.deps_paths[:ecto_sql]
+ecto_sql = "#{ecto_sql}/integration_test/sql"
+
+Code.require_file "#{ecto_sql}/alter.exs", __DIR__
+Code.require_file "#{ecto_sql}/lock.exs", __DIR__
+Code.require_file "#{ecto_sql}/logging.exs", __DIR__
+Code.require_file "#{ecto_sql}/migration.exs", __DIR__
+Code.require_file "#{ecto_sql}/migrator.exs", __DIR__
+Code.require_file "#{ecto_sql}/sandbox.exs", __DIR__
+Code.require_file "#{ecto_sql}/sql.exs", __DIR__
+Code.require_file "#{ecto_sql}/stream.exs", __DIR__
+Code.require_file "#{ecto_sql}/subquery.exs", __DIR__
+Code.require_file "#{ecto_sql}/transaction.exs", __DIR__
--- integration/mssql/all_ecto_test.exs
+++ integration/mssql/all_ecto_test.exs
@@ -8,17 +8,3 @@ Code.require_file "#{ecto}/preload.exs", __DIR__
 Code.require_file "#{ecto}/repo.exs", __DIR__
 Code.require_file "#{ecto}/type.exs", __DIR__
 Code.require_file "#{ecto}/windows.exs", __DIR__
-
-ecto_sql = Mix.Project.deps_paths[:ecto_sql]
-ecto_sql = "#{ecto_sql}/integration_test/sql"
-
-Code.require_file "#{ecto_sql}/alter.exs", __DIR__
-Code.require_file "#{ecto_sql}/lock.exs", __DIR__
-Code.require_file "#{ecto_sql}/logging.exs", __DIR__
-Code.require_file "#{ecto_sql}/migration.exs", __DIR__
-Code.require_file "#{ecto_sql}/migrator.exs", __DIR__
-Code.require_file "#{ecto_sql}/sandbox.exs", __DIR__
-Code.require_file "#{ecto_sql}/sql.exs", __DIR__
-Code.require_file "#{ecto_sql}/stream.exs", __DIR__
-Code.require_file "#{ecto_sql}/subquery.exs", __DIR__
-Code.require_file "#{ecto_sql}/transaction.exs", __DIR__
