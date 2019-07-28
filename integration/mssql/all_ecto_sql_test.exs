64a8511db902da2f482ac56e892137435a5c07a4
whossname <tyson.buzza@gmail.com>
2019-07-28 14:19:28 +0800

    correct use of Code.require_file

--- integration/mssql/all_ecto_sql_test.exs
+++ integration/mssql/all_ecto_sql_test.exs
@@ -1,18 +1,17 @@
-ecto_sql = Mix.Project.deps_paths[:ecto_sql]
+ecto_sql = Mix.Project.deps_paths()[:ecto_sql]
 ecto_sql = "#{ecto_sql}/integration_test/sql"
 
-Code.require_file "#{ecto_sql}/alter.exs", __DIR__
-Code.require_file "#{ecto_sql}/logging.exs", __DIR__
-Code.require_file "#{ecto_sql}/migration.exs", __DIR__
-Code.require_file "#{ecto_sql}/migrator.exs", __DIR__
-Code.require_file "#{ecto_sql}/sql.exs", __DIR__
-Code.require_file "#{ecto_sql}/subquery.exs", __DIR__
-
+Code.require_file("alter.exs", ecto_sql)
+Code.require_file("logging.exs", ecto_sql)
+Code.require_file("migration.exs", ecto_sql)
+Code.require_file("migrator.exs", ecto_sql)
+Code.require_file("sql.exs", ecto_sql)
+Code.require_file("subquery.exs", ecto_sql)
 
 # should support
-#Code.require_file "#{ecto_sql}/sandbox.exs", __DIR__
+#Code.require_file("sandbox.exs", ecto_sql)
 
 # Partial / No Support
-#Code.require_file "#{ecto_sql}/stream.exs", __DIR__
-#Code.require_file "#{ecto_sql}/transaction.exs", __DIR__
-#Code.require_file "#{ecto_sql}/lock.exs", __DIR__
+#Code.require_file("stream.exs", ecto_sql)
+#Code.require_file("transaction.exs", ecto_sql)
+#Code.require_file("lock.exs", ecto_sql)
--- integration/mssql/all_ecto_test.exs
+++ integration/mssql/all_ecto_test.exs
@@ -1,10 +1,12 @@
-ecto = Mix.Project.deps_paths[:ecto]
+ecto = Mix.Project.deps_paths()[:ecto]
 ecto = "#{ecto}/integration_test/cases"
 
-Code.require_file "#{ecto}/assoc.exs", __DIR__
-Code.require_file "#{ecto}/interval.exs", __DIR__
-Code.require_file "#{ecto}/joins.exs", __DIR__
-Code.require_file "#{ecto}/preload.exs", __DIR__
-Code.require_file "#{ecto}/repo.exs", __DIR__
-Code.require_file "#{ecto}/type.exs", __DIR__
-Code.require_file "#{ecto}/windows.exs", __DIR__
+Ecto.Integration.TestRepo.__info__(:compile)
+
+Code.require_file("assoc.exs", ecto)
+Code.require_file("interval.exs", ecto)
+Code.require_file("joins.exs", ecto)
+Code.require_file("preload.exs", ecto)
+Code.require_file("repo.exs", ecto)
+Code.require_file("type.exs", ecto)
+Code.require_file("windows.exs", ecto)
